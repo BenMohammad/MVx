3c7c4cb646e859b2e8cc720432848b8c925dd8f0
Ben Mohammad <benmohammad593@gmail.com>
2020-01-17 21:10:47 +0000

    QuestionDetailsResponseSchema added...

---
+++ app/src/main/java/com/fukuni/mvx/networking/QuestionDetailsResponseSchema.java
@@ -0,0 +1,20 @@
+package com.fukuni.mvx.networking;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionDetailsResponseSchema {
+
+    @SerializedName("items")
+    private final List<QuestionSchema> mQuestions;
+
+    public QuestionDetailsResponseSchema(QuestionSchema questions) {
+        this.mQuestions = Collections.singletonList(questions);
+    }
+
+    public QuestionSchema getQuestions() {
+        return mQuestions.get(0);
+    }
+}

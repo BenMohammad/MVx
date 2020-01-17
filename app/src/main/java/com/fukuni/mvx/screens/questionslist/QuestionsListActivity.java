19f9efc4b08d235fec53381da0f7bbdc3be75e47
Ben Mohammad <benmohammad593@gmail.com>
2020-01-17 14:02:16 +0000

    QuestionsListActivity and XML and Constants updated....

--- app/src/main/java/com/fukuni/mvx/screens/questionslist/QuestionsListActivity.java
+++ app/src/main/java/com/fukuni/mvx/screens/questionslist/QuestionsListActivity.java
@@ -1,16 +1,87 @@
 package com.fukuni.mvx.screens.questionslist;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Toast;
 
 import com.fukuni.mvx.R;
+import com.fukuni.mvx.common.Constants;
+import com.fukuni.mvx.networking.QuestionSchema;
+import com.fukuni.mvx.networking.QuestionsListResponseSchema;
+import com.fukuni.mvx.networking.StackoverflowAPI;
+import com.fukuni.mvx.questions.Question;
 import com.fukuni.mvx.screens.common.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class QuestionsListActivity extends BaseActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class QuestionsListActivity extends BaseActivity implements QuestionsListViewMvc.Listener {
+
+    private StackoverflowAPI mStackAPI;
+    private QuestionsListViewMvc mViewMvc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_questions_list);
+        mViewMvc = new QuestionsListViewMvcImpl(LayoutInflater.from(this), null);
+        mViewMvc.registerListener(this);
+
+        mStackAPI = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(StackoverflowAPI.class);
+
+        setContentView(mViewMvc.getRootview());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fetchQuestions();
+    }
+
+    private void fetchQuestions() {
+        mStackAPI.fetchLastActiveQuestions(Constants.QUESTIONS_LIST_PAGE_SIZE)
+                .enqueue(new Callback<QuestionsListResponseSchema>() {
+                    @Override
+                    public void onResponse(Call<QuestionsListResponseSchema> call, Response<QuestionsListResponseSchema> response) {
+                        if(response.isSuccessful()) {
+                            bindQuestions(response.body().getQuestions());
+                        } else {
+                            networkFailed();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<QuestionsListResponseSchema> call, Throwable t) {
+                        networkFailed();
+                    }
+                });
+    }
+
+    private void bindQuestions(List<QuestionSchema> questionSchemas) {
+        List<Question> questions = new ArrayList<>(questionSchemas.size());
+        for(QuestionSchema questionSchema : questionSchemas) {
+            questions.add(new Question(questionSchema.getId(), questionSchema.getTitle()));
+        }
+        mViewMvc.bindQuestions(questions);
+    }
+
+    private void networkFailed() {
+        Toast.makeText(this, "Network Error", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onQuestionClicked(Question question) {
+        Toast.makeText(this, question.getTitle(), Toast.LENGTH_SHORT).show();
     }
 }

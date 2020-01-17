b098f93f5f20781c7b4c94d00706dfaf26934cb9
Ben Mohammad <benmohammad593@gmail.com>
2020-01-17 13:25:35 +0000

    QuestionsRecyclerAdapter added...

---
+++ app/src/main/java/com/fukuni/mvx/screens/questionslist/QuestionsRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.fukuni.mvx.screens.questionslist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.fukuni.mvx.questions.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionsRecyclerAdapter extends RecyclerView.Adapter<QuestionsRecyclerAdapter.MyViewHolder> implements QuestionsListItemViewMvc.Listener {
+
+
+    public interface Listener {
+        void onQuestionClicked(Question question);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        private final QuestionsListItemViewMvc mViewMvc;
+
+        public MyViewHolder(QuestionsListItemViewMvc viewMvc) {
+            super(viewMvc.getRootview());
+            this.mViewMvc = viewMvc;
+        }
+
+    }
+
+    private final LayoutInflater mInflator;
+    private final Listener mListener;
+    private List<Question> mQuestions = new ArrayList<>();
+
+    public QuestionsRecyclerAdapter(LayoutInflater inflator, Listener listener) {
+        this.mInflator = inflator;
+        this.mListener = listener;
+    }
+
+    public void bindQuestions(List<Question> questions) {
+        mQuestions = new ArrayList<>(questions);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        QuestionsListItemViewMvc viewMvc = new QuestionsListItemViewMvcImpl(mInflator, parent);
+        viewMvc.registerListener(this);
+        return new MyViewHolder(viewMvc);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.mViewMvc.bindQuestion(mQuestions.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mQuestions.size();
+    }
+
+    @Override
+    public void onQuestionClicked(Question question) {
+        mListener.onQuestionClicked(question);
+    }
+}

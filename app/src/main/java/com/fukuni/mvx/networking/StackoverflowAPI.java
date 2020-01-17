4b5047cd018fcec9e97b6d406c970b981523410f
Ben Mohammad <benmohammad593@gmail.com>
2020-01-17 21:12:23 +0000

    StackoverflowAPI refactored updated ....

--- app/src/main/java/com/fukuni/mvx/networking/StackoverflowAPI.java
+++ app/src/main/java/com/fukuni/mvx/networking/StackoverflowAPI.java
@@ -4,11 +4,16 @@ import com.fukuni.mvx.common.Constants;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface StackoverflowAPI {
 
     @GET("/questions?key=" + Constants.STACKOVERFLOW_API_KEY + "&sort=activity&order=desc&site=stackoverflow&filter=withbody")
     Call<QuestionsListResponseSchema> fetchLastActiveQuestions(@Query("pagesize") Integer pageSize);
+
+    @GET("/questions/{questionId}?key=" + Constants.STACKOVERFLOW_API_KEY + "&site=stackoverflow&filter=withbody")
+    Call<QuestionDetailsResponseSchema> fetchQuestionDetails(@Path("questionId") String questionId);
+
 
 }

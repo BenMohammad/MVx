ef123dd3fdfc44f18aa8d4de91e4d947b751b71c
Ben Mohammad <benmohammad593@gmail.com>
2020-01-18 19:04:59 +0000

    XML backBtn also added to ToolbarViewMvc ...

--- app/src/main/java/com/fukuni/mvx/screens/common/toolbar/ToolbarViewMvc.java
+++ app/src/main/java/com/fukuni/mvx/screens/common/toolbar/ToolbarViewMvc.java
@@ -2,6 +2,7 @@ package com.fukuni.mvx.screens.common.toolbar;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.fukuni.mvx.R;
@@ -10,10 +11,12 @@ import com.fukuni.mvx.screens.common.views.BaseViewMvc;
 public class ToolbarViewMvc extends BaseViewMvc {
 
     private final TextView mTxtTitle;
+    private final ImageButton mBtnBack;
 
     public ToolbarViewMvc(LayoutInflater inflater, ViewGroup parent) {
         setRootview(inflater.inflate(R.layout.layout_toolbar, parent, false));
         mTxtTitle = findViewById(R.id.txt_toolbar_title);
+        mBtnBack = findViewById(R.id.btn_back);
     }
 
     public void setTitle(String title) {
